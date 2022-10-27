98be0caccd66b3bbf4c29aad6d601c1c51bc7881
Tigris <tigeysky@gmail.com>
2022-10-27 15:52:39 -0400

    Update vision1.py

--- vision1.py
+++ vision1.py
@@ -3,6 +3,7 @@ import rospy
 import tf
 from geometry_msgs.msg import Pose, PoseStamped, Twist, Quaternion
 from mavros_msgs.msg import OverrideRCIn
+from sensor_msgs.msg import Image
 from mavros_msgs.msg import RCIn
 from mavros_msgs.srv import CommandBool
 from mavros_msgs.srv import SetMode
@@ -22,7 +23,7 @@ class MavController:
         rospy.init_node("mav_control_node")
         rospy.Subscriber("/mavros/local_position/pose", PoseStamped, self.pose_callback)
         rospy.Subscriber("/mavros/rc/in", RCIn, self.rc_callback)
-        rospy.Subscriber("/camera/imageraw", ImageIn, self.image_callback)
+        rospy.Subscriber("/camera/imageraw", Image, self.image_callback)
 
         self.cmd_pos_pub = rospy.Publisher("/mavros/setpoint_position/local", PoseStamped, queue_size=1)
         self.cmd_vel_pub = rospy.Publisher("/mavros/setpoint_velocity/cmd_vel_unstamped", Twist, queue_size=1)
@@ -37,7 +38,7 @@ class MavController:
 
         self.rc = RCIn()
         self.pose = Pose()
-        self.cv_image = ImageIn()
+        self.cv_image = Image()
         self.timestamp = rospy.Time()
 
     def rc_callback(self, data):
